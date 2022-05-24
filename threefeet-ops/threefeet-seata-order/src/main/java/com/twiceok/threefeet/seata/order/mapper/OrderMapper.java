08480a33ae0ce354a6344d772045b8f8300e2fb4
lqf <1335121729@qq.com>
2022-05-25 00:55:00 +0800

    commit init

---
+++ threefeet-ops/threefeet-seata-order/src/main/java/com/twiceok/threefeet/seata/order/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.twiceok.threefeet.seata.order.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.twiceok.threefeet.seata.order.entity.Order;
+
+/**
+ * OrderMapper
+ *
+ * @author Chill
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+}
---
+++ threefeet-ops/threefeet-seata-storage/src/main/java/com/twiceok/threefeet/seata/storage/mapper/StorageMapper.java
@@ -0,0 +1,12 @@
+package com.twiceok.threefeet.seata.storage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.twiceok.threefeet.seata.storage.entity.Storage;
+
+/**
+ * StorageMapper
+ *
+ * @author Chill
+ */
+public interface StorageMapper extends BaseMapper<Storage> {
+}
